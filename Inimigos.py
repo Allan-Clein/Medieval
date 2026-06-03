f8d3f75eaed91bfaadb76b376b502a3ac1d4ad0c
Allan Clein <allanclein2@gmail.com>
2026-06-03 15:51:50 -0300

    Add files via upload

---
+++ Inimigos.py
@@ -0,0 +1,66 @@
+import random
+idano = 0
+ivida = 0
+idefesa = 0
+danototal = 0
+Encontrados = 0
+amplitude = 0
+import threading
+import Bestiary
+import viagem as v
+
+aptos = []
+
+#-----------------------------------------------------------Inicial----------------------------------------------------------#
+
+slime_iniciante = {"nome" : "Slime Iniciante",
+                       "vida" : 50,
+                       "dano" : 15,
+                       "amplitude" : 5,
+                       "encontrado" : 0,
+                       "elemento" : False}
+
+#-----------------------------------------------------------Montanha Ophidia----------------------------------------------------------#
+
+gnomo_petrificado = {"nome" : "Gnomo Petrificado",
+                     "vida" : 80,
+                     "dano" : 15,
+                     "amplitude" : 10,
+                     "defesa" : 15/100,
+                     "encontrado" : 0,
+                     "elemento" : "geodo"}
+
+serpente_alada = {"nome" : "Serpente Alada",
+                     "vida" : 30,
+                     "dano" : 25,
+                     "amplitude" : 5,
+                     "defesa" : 0/100,
+                     "encontrado" : 0,
+                     "elemento" : "anemoria"}
+
+#-----------------------------------------------------------Floresta Raizal----------------------------------------------------------#
+
+raiz_enfurecida = {"nome" : "Raiz Enfurecida",
+                     "vida" : 80,
+                     "dano" : 15,
+                     "amplitude" : 10,
+                     "defesa" : 0/100,
+                     "encontrado" : 0,
+                     "elemento" : "natura"}
+
+inimigos = []
+
+def pode_cair():
+    global aptos
+    if v.local == "Floresta Raizal.":
+        aptos = [raiz_enfurecida]
+    if v.local == "Montanha Ophidia.":
+        aptos = [gnomo_petrificado,serpente_alada]
+
+
+def slime_inicial():
+    global idano, ivida, danototal, Encontrados
+    global slime_iniciante
+    idano = slime_iniciante['dano']
+    ivida = slime_iniciante['vida']
+    danototal = 0
---
+++ battle.py
@@ -0,0 +1,9 @@
+import Bestiary as Bt
+import os
+import Inimigos as Iny
+import random as rn
+
+#Aqui vai ficar o sistema de ataques que cada inimigo pode utilizar, usando listas serpente alada tem [ataques]
+
+def clear():
+    os.system('cls')
