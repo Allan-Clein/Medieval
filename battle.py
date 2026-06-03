e1ed151e2b441236deb651e56f94d3529f0bfff4
Allan Clein <allanclein2@gmail.com>
2026-06-03 16:01:57 -0300

    Delete battle.py

--- battle.py
+++
@@ -1,9 +0,0 @@
-import Bestiary as Bt
-import os
-import Inimigos as Iny
-import random as rn
-
-#Aqui vai ficar o sistema de ataques que cada inimigo pode utilizar, usando listas serpente alada tem [ataques]
-
-def clear():
-    os.system('cls')
